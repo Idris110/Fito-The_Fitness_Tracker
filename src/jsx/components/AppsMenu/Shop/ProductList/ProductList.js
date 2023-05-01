61931cc6168a7505a38c3172590fa8b25bd900ea
Idris110 <ratlamwalaidris110@gmail.com>
2023-05-01 23:58:11 +0530

    exercise data

--- src/jsx/components/AppsMenu/Shop/ProductList/ProductList.js
+++ src/jsx/components/AppsMenu/Shop/ProductList/ProductList.js
@@ -1,15 +1,16 @@
 import React, { Fragment } from "react";
 import SingleProductList from "./SingleProductList";
-import productData from "../productData";
+// import productData from "../productData";
+import exData from "../productData/exerciseData";
 import PageTitle from "../../../../layouts/PageTitle";
 
 const ProductList = () => {
    return (
       <Fragment>
-         <PageTitle activeMenu="Blank" motherMenu="Layout" />
+         <PageTitle activeMenu="Exercises" motherMenu="Workout" />
 
          <div className="row">
-            {productData.map((product) => (
+            {exData.map((product) => (
                <SingleProductList key={product.key} product={product} />
             ))}
          </div>
